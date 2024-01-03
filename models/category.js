c004b4ad6e929d4c6e5d9b68f61eed5abf99310c
Pavel <45903339+Pavel20011987@users.noreply.github.com>
2024-01-04 01:25:41 +0300

    Part_2

--- models/category.js
+++ models/category.js
@@ -1,6 +1,22 @@
 const mongoose = require('mongoose');
 
 const categorySchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    color: {
+        type: String,
+        required: false
+
+    },
+    icon: {
+        type: String,
+        required: false
+    },
+    image: {
+        type: String
+    }
 
 })
 
--- routers/products.js
+++ routers/products.js
@@ -1,5 +1,7 @@
 const { Product } = require('../models/products');
+const { Category } = require('../models/category');
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
 
 router.get(`/`, async(req, res) => {
@@ -11,21 +13,93 @@ router.get(`/`, async(req, res) => {
     res.send(productList);
 })
 
-router.post(`/`, (req, res) => {
-    const product = new Product({
+router.get(`/:id`, async(req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        res.status(500).json({ success: false })
+    }
+    res.send(product);
+})
+
+router.post(`/`, async(req, res) => {
+    const category = await Category.findById(req.body.category);
+    if (!category) return res.status(400).send('Invalid Category');
+
+    let product = new Product({
         name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
         image: req.body.image,
-        countInStock: req.body.countInStock
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured,
+        //dateCreated: req.body.dateCreated
     })
 
-    product.save().then((createdProduct => {
-        res.status(201).json(createdProduct)
-    })).catch((err) => {
-        res.status(500).json({
-            error: err,
-            success: false
-        })
+    product = await product.save();
+
+    if (!product)
+        return res.status(500).send('The product cannot be created');
+
+    res.send(product);
+})
+
+router.put('/:id', async(req, res) => {
+    if (!mongoose.isValidObjectId(req.params.id)) {
+        res.status(400).send('Invalid Product Id');
+    }
+    const category = await Category.findById(req.body.category);
+    if (!category) return res.status(400).send('Invalid Category');
+
+    const product = await Product.findByIdAndUpdate(
+        req.params.id, {
+            name: req.body.name,
+            description: req.body.description,
+            richDescription: req.body.richDescription,
+            image: req.body.image,
+            brand: req.body.brand,
+            price: req.body.price,
+            category: req.body.category,
+            countInStock: req.body.countInStock,
+            rating: req.body.rating,
+            numReviews: req.body.numReviews,
+            isFeatured: req.body.isFeatured,
+            //dateCreated: req.body.dateCreated
+        }, { new: true }
+    )
+
+    if (!product)
+        return res.status(500).send('the product cannot be updated!');
+
+    res.send(product);
+})
+
+router.delete('/:id', (req, res) => {
+    Product.findByIdAndDelete(req.params.id).then(product => {
+        if (product) {
+            return res.status(200).json({ success: true, message: 'the product is deleted!' });
+        } else {
+            return res.status(404).json({ success: false, message: "product not found!" });
+        }
+    }).catch(err => {
+        return res.status(400).json({ success: false, error: err });
     })
+})
+
+router.get('/get/count', async(req, res) => {
+    const productCount = await Product.countDocuments();
+
+    if (!productCount) {
+        res.status(500).json({ success: false })
+    }
+    res.send({
+        productCount: productCount
+    });
 })
 
 module.exports = router;
